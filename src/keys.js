8ae3847dba2b39066ebeea4dcd423f9ea48fddd1
simja <simja@example.com>
2024-06-23 17:16:34 -0600

    update

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     database: {
         // URI: 'mongodb://localhost/inclutechSN'
-        URI: "mongodb+srv://inclutechAdmin:foYND5yURoxS63pR@cluster0.awaseaa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        URI: "mongodb+srv://inclutechAdmin:foYND5yURoxS63pR@cluster0.awaseaa.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0"
     }
 }
