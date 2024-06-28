cd208225f6a26712701e7b8d6a92662a1b715cab
Simja <yorkamacua@gmail.com>
2024-06-28 13:29:14 -0600

    url key

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     database: {
-        URI: 'mongodb://localhost/inclutechSN'
-        // URI: "mongodb+srv://inclutechUser:lqeNzTJdeI9qNox2@cluster0.vbrdurz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        // URI: 'mongodb://localhost/inclutechSN'
+        URI: "mongodb+srv://inclutechUser:lqeNzTJdeI9qNox2@cluster0.vbrdurz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     }
 }
